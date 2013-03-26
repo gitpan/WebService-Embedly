69bdea3946aed5fd6bbde3dfe33c4ae95bd44fce
Jason Wieland <jwieland@cpan.org>
2013-03-26 03:59:06 +0000

    Import of JWIELAND/WebService-Embedly-0.08 from CPAN.
    gitpan-cpan-distribution: WebService-Embedly
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         JWIELAND/WebService-Embedly-0.08.tar.gz
    gitpan-cpan-author:       JWIELAND
    gitpan-cpan-maturity:     released

--- lib/WebService/Embedly.pm
+++ lib/WebService/Embedly.pm
@@ -12,7 +12,7 @@ use JSON;
 use URI::Escape;
 use Ouch qw(:traditional);
 use Regexp::Common qw /URI/;
-our $VERSION = '0.05';
+our $VERSION = '0.04';
 
 =head1 NAME
 
@@ -20,7 +20,7 @@ WebService::Embedly - Perl interface to the Embedly API
 
 =head1 VERSION
 
-Version 0.05
+Version 0.04
 
 =cut
 
@@ -367,12 +367,6 @@ sub _request {
   my $embed_url = $in->{embed_url};
   my $base_uri  = $in->{base_uri};
 
-  #quick check to see if we have a url
-  unless ( $embed_url ) {
-    throw 400, 'Missing embed URL does not look like a properly formatted url: ';
-  }
-
-
   my %params = ( key => $self->api_key );
 
   if (ref($embed_url) eq 'ARRAY') {
@@ -386,15 +380,16 @@ sub _request {
       unless ( $RE{URI}{HTTP}->{-scheme => qr/https?/}->matches($e_url) ) {
 	throw 400, 'Look-up URL does not look like a properly formatted url: ' . $e_url;
       }
-      push @escaped_urls, uri_escape($e_url) ;
+      push @escaped_urls, uri_escape_utf8($e_url) ;
     }
     $params{urls} = join (',', @escaped_urls);
   }
   else {
+    #quick check to see if we have a url
     unless ( $RE{URI}{HTTP}->{-scheme => qr/https?/}->matches($embed_url) ) {
       throw 400, 'Look-up URL does not look like a properly formatted url: ' . $embed_url;
     }
-    $params{url} = uri_escape($embed_url);
+    $params{url} = uri_escape_utf8($embed_url);
   }
 
 #  can't use query_form because embedly can't handle full application/x-www-form-urlencoded for multi urls (the comma between urls needs to stay a comma and not escaped to %2C) boo
@@ -489,12 +484,7 @@ Jason Wieland, C<< <jwieland at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-www-embedly at rt.cpan.org>, or through
-the web interface at L<https://github.com/jwieland/embedly-perl/issues>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Please report any bugs or feature requests through the web interface at L<https://github.com/jwieland/embedly-perl/issues>.  I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
 
 =head1 SUPPORT
 
@@ -502,8 +492,7 @@ You can find documentation for this module with the perldoc command.
 
     perldoc WebService::Embedly
 
-
-You can also look for information at:
+You can also look for information at: https://github.com/jwieland/embedly-perl
 
 =over 4
 
@@ -527,8 +516,8 @@ by the Free Software Foundation; or the Artistic License.
 
 See L<http://dev.perl.org/licenses/> for more information.
 
-
 =cut
+
 __PACKAGE__->meta->make_immutable();
 
 1; # End of WebService::Embedly
